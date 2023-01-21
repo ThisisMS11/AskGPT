8b0378b0c9103ae0b96dd21d934e1e037e459717
Vaibhav Singh <21bcs232@iiitdmj.ac.in>
2023-01-21 17:19:20 +0530

    made login and signup pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
 import Comments from './components/comment'
 import { Route, Routes } from 'react-router'
 import Home from './components/home/home'
+import SignUp from './components/auth/index'
+import  Login from './components/login/index'
 function App() {
 
   return (
@@ -8,6 +10,9 @@ function App() {
       <Routes>
         <Route path="/replies/:id" element={<Comments />} />
         <Route path="/" element={<Home />} />
+        <Route path='/login' element = {<Login/>}/>
+
+        <Route path='/signup' element= {<SignUp/>}/>
       </Routes>
     </div>
   )
